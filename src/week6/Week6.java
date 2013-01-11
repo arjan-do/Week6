02c317480b4c1b0cc47ab2f0073e7829a59170f8
arjan-do <arjan-doets@live.nl>
2013-01-11 11:58:43 +0100

    init repo

---
+++ src/week6/Week6.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week6;
+
+/**
+ *
+ * @author arjan
+ */
+public class Week6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
